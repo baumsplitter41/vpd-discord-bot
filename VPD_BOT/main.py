3bf3507fe76edebe1865310f8170817ed0cf3582
BaumSplitter41 <BaumSplitter41@gmail.com>
2025-12-29 18:21:43 +0100

    fixing sa command

--- VPD_BOT/main.py
+++ VPD_BOT/main.py
@@ -181,15 +181,7 @@ async def greet(ctx, user: str = Option(discord.User, "The user, you want to gre
 
 #---------------------------------#
 ## Say
-"""@bot.slash_command(description="Let the bot send a message")
-async def say(
-        ctx,
-        text: str = Option(description="Input the text you want to send"),
-        channel_input: discord.TextChannel = Option(description="Select the channel,where you want to send the message.")
-):  
-    channel= discord.utils.get(ctx.guild.channels, id = int(channel_input[2:-1]))
-    await channel.send(text)
-    await ctx.respond("Message sent", ephemeral=True)"""
+
 #---------------------------------#
 
 #---------------------------------#
