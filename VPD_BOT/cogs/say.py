0f48e4046d638001562af376535e4007b1723b54
BaumSplitter41 <BaumSplitter41@gmail.com>
2025-12-29 10:14:46 +0100

    added /say as cog module

---
+++ VPD_BOT/cogs/say.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+from discord.commands import Option
+from discord.commands import slash_command
+
+
+class Base(commands.Cog):
+    def __init__(self, bot: discord.Bot):
+        self.bot = bot
+
+    @slash_command(description="Greet a User")
+    async def greet(ctx, user: str = Option(discord.User, "The user, you want to greet")):
+        await ctx.respond(f"Hello {user.mention}")
+
+
+def setup(bot: discord.Bot):
+    bot.add_cog(Base(bot))
