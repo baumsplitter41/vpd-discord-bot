43ee5ffa4c3ebf193cff153b2424172618bd275a
BaumSplitter41 <BaumSplitter41@gmail.com>
2025-12-29 23:30:54 +0100

    deleting /userinfo from main.py

--- VPD_BOT/main.py
+++ VPD_BOT/main.py
@@ -179,46 +179,6 @@ async def greet(ctx, user: str = Option(discord.User, "The user, you want to gre
     await ctx.respond(f"Hello {user.mention}")
 #---------------------------------#
 
-#---------------------------------#
-## Userinfo
-@bot.slash_command(name="userinfo", description="Show informations of a user from this server")
-async def userinfo(
-        ctx,
-        user: str = Option(discord.User, "Select User"),
-    ):
-    if user is None:
-        user = ctx.author
-    elif user not in ctx.guild.members:
-        await ctx.respond("The selected user is not a member on this Server!", ephemeral=True)
-        return
-    elif user == bot.user:
-        await ctx.respond(f"This is me - the {bot.user}", ephemeral=True)
-        return
-
-    embed = discord.Embed(
-        title=f"Information about *{user.name}*",
-        description=f"Here you see all details about {user.mention}",
-        color=discord.Color.blue()
-    )
-
-    time = discord.utils.format_dt(user.created_at, "R")
-
-    embed.add_field(name="Account creation date", value=time, inline=False)
-    if len(user.roles) >= 2:
-        embed.add_field(name="Roles", value=", ".join([role.mention for role in user.roles if role.name != "@everyone"]), inline=False)
-    else:
-        embed.add_field(name="Roles", value="User has no roles", inline=False)
-    embed.add_field(name="Server join date", value=discord.utils.format_dt(user.joined_at, "R"), inline=False)
-    embed.add_field(name="User ID", value=user.id)
-
-    embed.set_thumbnail(url=user.display_avatar.url)
-    embed.set_author(name="VicePD", icon_url="https://i.imgur.com/6QteFrg.png")
-    embed.set_footer(text="VicePD - Bot | Made by BaumSplitter41")
-
-    await ctx.respond(embed=embed)
-#---------------------------------#
-
-
 #_________________________________#
 #BAN SYSTEM
 
