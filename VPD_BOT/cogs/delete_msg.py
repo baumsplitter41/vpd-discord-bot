93fcaa8c2d847f685153a10fc441eae408eb9c3e
BaumSplitter41 <baumsplitter41@gmail.com>
2026-01-22 17:55:28 +0100

    Fixing errors

--- VPD_BOT/cogs/delete_msg.py
+++ VPD_BOT/cogs/delete_msg.py
@@ -23,13 +23,12 @@ class delete(commands.Cog):
             await ctx.respond("You don't have the permission to use this command!", ephemeral=True)
             return
         deleted_messages = []
-        async for msg in ctx.channel.history(limit=amount + 1):
+        async for msg in ctx.channel.history(limit=amount):
             deleted_messages.append(msg)
-            print(deleted_messages)
-        if len(deleted_messages) <= 1:
+        if len(deleted_messages) == 0:
             await ctx.respond("No messages found to delete.", ephemeral=True)
             return
-        await ctx.channel.delete_messages(deleted_messages[1:])
+        await ctx.channel.delete_messages(deleted_messages)
         await ctx.respond(f"Deleted {len(deleted_messages) - 1} messages.", ephemeral=True)
 
 def setup(bot: discord.Bot):
