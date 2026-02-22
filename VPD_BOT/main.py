30bd9e1a108756e48ebcd97110bfc05cd55fa2be
baumsplitter41 <baumsplitter41@gmail.com>
2026-02-22 13:30:11 +0100

    placed the welcome message in the main.py

--- VPD_BOT/main.py
+++ VPD_BOT/main.py
@@ -585,6 +585,35 @@ async def update_users_periodically():
         await asyncio.sleep(60)  # Update every minute
 
 
+#_________________________________#
+#Welcome Message
+
+@commands.Cog.listener()
+async def on_member_join(self, member: discord.Member):
+    config = configparser.ConfigParser()
+    configFilePath = r'config.cfg'
+    config.read(configFilePath)
+    enable_welcome_message = config.getboolean("Welcome Message","enable_welcome_message")
+    if not enable_welcome_message:
+        return
+    
+    welcome_channel_id = config.getint("Welcome Message","welcome_channel_id")
+    welcome_channel = self.bot.get_channel(welcome_channel_id)
+    if welcome_channel is None:
+        print(f"Welcome channel with ID {welcome_channel_id} not found.")
+        return
+    
+    embed = discord.Embed(
+        title="VicePD",
+        description=f"Willkommen {member.mention}, auf **VicePD**! \n\n Bitte lese dir das <#1442279753707946215> durch. Die Einhaltung der Regeln stellt sicher, dass der Server ein freundlicher und unterhaltsamer Ort für alle ist und bleibt. \n\n Viel Spaß auf VicePD!",
+        color=discord.Color.grey(),
+        timestamp=discord.utils.utcnow()
+    )
+    embed.set_image(url="https://i.imgur.com/iu1VyKZ.png")
+    embed.set_footer(text=f"Member ID: {member.id}")
+    
+    self.bot.loop.create_task(welcome_channel.send(embed=embed))
+    #await welcome_channel.send(embed=embed) 
 
 
 
