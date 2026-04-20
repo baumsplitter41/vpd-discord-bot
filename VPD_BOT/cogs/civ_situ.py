64b997de810d6caec1f36ef720b93c673a45b786
BaumSplitter41 <BaumSplitter41@gmail.com>
2026-04-20 09:42:08 +0200

    fixing

--- VPD_BOT/cogs/civ_situ.py
+++ VPD_BOT/cogs/civ_situ.py
@@ -13,6 +13,7 @@ class civsitu(commands.Cog):
     class Situ(discord.ui.Modal):
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
+            self.bot = kwargs.get("bot")
 
             #Modal form layout
             self.add_item(discord.ui.InputText(label="Teilnehmende Spieler", placeholder="Gib die Namen der teilnehmenden Spieler abgesehen von dir ein.", required=False, ))
