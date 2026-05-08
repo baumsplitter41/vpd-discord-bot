0dcd9af170050f174fa5706c1355841eb7662bc1
BaumSplitter41 <BaumSplitter41@gmail.com>
2026-05-08 10:21:12 +0200

    fixing

--- VPD_BOT/cogs/reaction_roles.py
+++ VPD_BOT/cogs/reaction_roles.py
@@ -149,7 +149,7 @@ class Reactionroles(commands.Cog):
                 if role not in member.roles:
                     try:
                         await member.add_roles(role)
-                        await ctx.response.send_message(f"The role {role.name} has been removed from your roles.", ephemeral=True)
+                        #await interaction.response.send_message(f"The role {role.name} has been removed from your roles.", ephemeral=True)
 
                     except Exception as e:
                         print(f"Failed to add role {role.name} to user {member.name}: {e}")
@@ -159,8 +159,7 @@ class Reactionroles(commands.Cog):
                 else:
                     try:
                         await member.remove_roles(role)
-                        #await member.send(f"The role {role.name} has been removed from your roles.", ephemeral=True)
-                        await ctx.response.send_message(f"The role {role.name} has been removed from your roles.", ephemeral=True)
+                        #await interaction.response.send_message(f"The role {role.name} has been removed from your roles.", ephemeral=True)
                     except Exception as e:
                         print(f"Failed to remove role {role.name} from user {member.name}: {e}")
                         break
